d0710866998c8cbb92fd3c915c98c1aee059afc9
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-04-02 22:56:06 +0100

    Adjust subcluster connections

--- code/functions.R
+++ code/functions.R
@@ -20,6 +20,7 @@ prep_graph <- function(file=here::here("data","contributors.xlsx")){
             data.table::data.table()
     )[(!is.na(from) & !is.na(to)),]
     edges_dt[,from:=sapply(from, prep_strings)][,to:=sapply(to,prep_strings)] 
+    edges_dt[is.na(hidden)]$hidden <- FALSE
     #### Nodes ####
     nodes_dt <- xlsx::read.xlsx(file, sheetName = "nodes") |>
             data.table::data.table()
@@ -40,19 +41,24 @@ prep_graph <- function(file=here::here("data","contributors.xlsx")){
                                    role=toString(unique(role)),
                                    name=toString(unique(name)),
                                    cluster=list(unique(cluster)),
-                                   cluster_str=paste(unique(cluster),collapse =";"),
+                                   cluster_str=paste(unique(cluster),
+                                                     collapse =";"),
+                                   subcluster=unique(subcluster),
                                    gradient=unique(na.omit(gradient)),
-                                   show=unique(show)
+                                   hidden=unique(hidden),
+                                   mult=ifelse(is.na(mult),1,mult)
                                 ),
-                          by="key"][,show:=ifelse(is.na(show),TRUE,show)] 
+                          by="key"]
+    nodes_dt_agg[,hidden:=ifelse(is.na(hidden),FALSE,hidden)] 
     nodes_dt_agg$i <- seq_len(nrow(nodes_dt_agg))
+    min_alpha <- .35
     nodes_dt_agg <- nodes_dt_agg |>  
         dplyr::group_by(cluster_str) |> 
         dplyr::mutate(gradient_i=ifelse(!is.na(gradient) & gradient, 
-                                        (dplyr::row_number()/dplyr::n())+.1,
+                                        (dplyr::row_number()/dplyr::n())+min_alpha,
                                         .75)) |>
         data.table::data.table()
-    # #### Assign node colors #### 
+    #### Assign node colors ####
     clusters_unique <- unique(unlist(nodes_dt_agg[[color_var]]))
     ncolors <- length(clusters_unique)
     my_palette <- stats::setNames(pals::ocean.phase(ncolors+1)[-1],
@@ -65,38 +71,48 @@ prep_graph <- function(file=here::here("data","contributors.xlsx")){
         ggplot2::alpha(my_palette[as.character(x[[idx]])], 1)
     })]
     #### Set shape ####
-    nodes_dt_agg[,shape:=ifelse(show,"dot","text")]
+    nodes_dt_agg[,shape:=ifelse(hidden,"text","dot")]
     #### Add hover info ####
     nodes_dt_agg[,title:=paste0(
         "<strong>entity</strong>: ",entity,"<br>",
         ifelse(role=="NA","",paste0("<strong>role</strong>: ",role,"<br>")),
-        "<strong>cluster</strong>: ",cluster,"<br>",
-        "<strong>color</strong>: ",color,"<br>",
-        "<strong>border</strong>: ",border,"<br>"
+        "<strong>cluster</strong>: ",cluster,"<br>"
+        # "<strong>color</strong>: ",color,"<br>",
+        # "<strong>border</strong>: ",border,"<br>"
     )] 
     #### Sort by cluster ####
     nodes_dt_agg <- suppressWarnings(
         nodes_dt_agg[order(as.numeric(nodes_dt_agg$cluster_str)),]
-    )
-    
+    ) 
     #### Add all possible connections within clusters ##### 
-    extra_edges <- lapply(unique(nodes_dt_agg$cluster),
+    extra_edges1 <- lapply(unique(nodes_dt_agg$cluster),
                           function(clust){
         nodes <- unique(nodes_dt_agg[cluster==clust,]$key)
         cj <- data.table::CJ(from=nodes,to=nodes,unique = TRUE)     
         cj[from!=to][,hidden:=TRUE]
     }) |> data.table::rbindlist()
-    edges_dt <- data.table::rbindlist(list(edges_dt[,hidden:=FALSE],
-                                           extra_edges), fill = TRUE)
-    
+    extra_edges2 <- lapply(unique(nodes_dt_agg$subcluster),
+                          function(clust){
+          nodes <- unique(nodes_dt_agg[subcluster==clust,]$key)
+          cj <- data.table::CJ(from=nodes,to=nodes,unique = TRUE)     
+          cj[from!=to][,hidden:=TRUE] 
+    }) |> data.table::rbindlist()
+    edges_dt <- data.table::rbindlist(list(edges_dt,
+                                           extra_edges1,
+                                           extra_edges2), 
+                                      fill = TRUE)
+    # edges_dt <- edges_dt[!duplicated(paste(edges_dt$from,edges_dt$to))]
     #### Convert to graph ####
     g <- igraph::graph_from_data_frame(d = edges_dt, 
-                                       vertices = nodes_dt_agg) 
-    g <- tidygraph::as_tbl_graph(g) 
+                                       vertices = nodes_dt_agg) #|>
+    g <- tidygraph::as_tbl_graph(g)
     #### Compute the number of connections per node ####
     igraph::V(g)$connections <- igraph::degree(g,mode = "all")
-    igraph::V(g)$value <- igraph::V(g)$connections
+    #### Add size ####
+    igraph::V(g)$value <- igraph::V(g)$connections*igraph::V(g)$mult 
     color_var <- "cluster" 
+    
+    # View(tidyr::as_tibble(g))
     # plot(g)
     return(g)
 }
@@ -108,12 +124,13 @@ plot_graph <- function(g,
                        solver = "forceAtlas2Based",
                        physics = FALSE,
                        forceAtlas2Based = list(
-                           avoidOverlap=1,
+                           avoidOverlap=1.5,
                            gravitationalConstant=-500),
                        show_plot = TRUE,
                        main = "Collaboration network",
                        submain = NULL,
                        background = "rgb(8,36,81)",
+                       open=FALSE,
                        randomSeed = 2023){
     # devoptera::args2vars(plot_graph, packages="dplyr")
     #### Create plot ####
@@ -125,18 +142,18 @@ plot_graph <- function(g,
                     c(., list(main = list(text=main,
                                           style="color:white"),
                               submain = submain,
-                              height = 14043/8,
-                              width = 9933/8,
+                              height = 14033/8,
+                              width = 14043/8,
                               background = background)
                     )
             )
         } |>
         visNetwork::visIgraphLayout(layout = "layout_with_graphopt",
                                     type = "full",
-                                    charge = .008,
+                                    charge = .080,
                                     # mass = 100,
                                     # spring.length = 50,
-                                    spring.constant = 0.1,
+                                    spring.constant = 1,
                                     physics = physics,
                                     randomSeed = randomSeed) |>
         # visNetwork::visLayout(improvedLayout = TRUE,
@@ -195,5 +212,7 @@ plot_graph <- function(g,
                             selfcontained = TRUE,
                             background = background)
     } 
-    return(vn)
+    if(isTRUE(open)) utils::browseURL(save_path)
+    return(list(plot=vn,
+                path=save_path))
 }
